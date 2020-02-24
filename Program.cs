ae706b291943d60b927b251cc4ccea10a62218c0
Robert Rydlewski <robertrydlewski1984@gmail.com>
2020-02-24 21:48:37 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+
+    ststic void MyMethod() 
+    {
+        Console.WriteLine("total in MyMethod:");
+        Condole.WriteLine(total);
+    }
+    static void Main(string[] args)
+    {
+        int total = 0;
+        total += 10;
+        Console.WriteLine("total in Main:");
+
+    }
+}
